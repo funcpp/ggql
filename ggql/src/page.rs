2865e090d6af2319ff020897aac283e18d63bede
funcpp <48622976+funcpp@users.noreply.github.com>
2025-01-16 01:25:35 +0900

    fix

--- ggql/src/page.rs
+++ ggql/src/page.rs
@@ -1,20 +1,21 @@
 use std::borrow::Cow;
 
-use async_graphql::{InputObject, OutputType, TypeName};
+use async_graphql::{InputObject, OutputType, SimpleObject, TypeName};
 
-#[derive(InputObject)]
+#[derive(InputObject, SimpleObject)]
 pub struct PageArguments {
     pub offset: i32,
     pub limit: i32,
 }
 
-pub struct Page<T> {
+#[derive(SimpleObject)]
+pub struct Page<T: OutputType> {
     pub items: Vec<T>,
     pub total: i32,
     pub arguments: Option<PageArguments>,
 }
 
-impl<T> Page<T> {
+impl<T: OutputType> Page<T> {
     pub fn new(items: Vec<T>, total: i32, arguments: Option<PageArguments>) -> Self {
         Self {
             items,
